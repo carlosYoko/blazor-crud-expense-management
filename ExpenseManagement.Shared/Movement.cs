6faa43c0b303f1a021b5d210f946d10343ebcb53
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-29 17:23:47 +0200

    created Movement and User entities

---
+++ ExpenseManagement.Shared/Movement.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpenseManagement.Shared
+{
+    public class Movement
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public double Quantity { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public User? User { get; set; }
+        public int UserId { get; set; }
+    }
+}
